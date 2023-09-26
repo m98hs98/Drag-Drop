e2a2aeb5c737152b246102558823807538cc921a
Mahsa <mahsahaddadi19@gmail.com>
2023-09-26 20:12:37 +1000

    Troubleshooting

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
 
 module.exports = {
     mode: 'production',
@@ -22,6 +26,16 @@ module.exports = {
         extensions: ['.ts', '.js']
     },
     plugins: [
-        new CleanPlugin.CleanWebpackPlugin()
+        new CleanPlugin.CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html',  // source file
+            filename: 'index.html'  // destination file
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: '_redirects', to: '' } // Copy _redirects to the root of `dist`
+            ],
+        })        
+        
     ]
 };
